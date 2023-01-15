2d716a76043a5a9f39e0dcc23bc676168bea91fa
PedroGomes2k <ph.gomes2k@gmail.com.br>
2023-01-15 06:33:27 -0300

    Cartas virando e fotos em nova pasta!

--- script.js
+++ script.js
@@ -1,8 +1,78 @@
 
+const containerDasCartas = document.querySelector('.containerCards')
+
+const imagensCartas = [
+    'bobrossparrota',
+    'explodyparrota',
+    'fiestaparrota',
+    'metalparrota',
+    'revertitparrota',
+    'tripletsparrota',
+    'unicornparrota'
+
+]
+
+function cartasCriadas(imagensSelecionadas) {
+
+
+    const cards = document.createElement('div')
+    const front = document.createElement('div')
+    const back = document.createElement('div')
+
+    cards.className = 'cards'
+    front.className = 'front card'
+    back.className = 'back '
+
+    front.style.backgroundImage = `url(../Imagens/back.png)`
+    back.style.backgroundImage = `url(../Imagens/${imagensSelecionadas}.gif)`
+    cards.addEventListener
+
+    cards.appendChild(front)
+    cards.appendChild(back)
+
+    containerDasCartas.appendChild(cards)
+}
+
+
+function jogabilidadeCartas() {
+
+    const cartasDuplicadas = [...imagensCartas, ...imagensCartas]
+
+    const cartasMisturadas = cartasDuplicadas.sort(() => Math.random() -0.2 )
+
+    cartasMisturadas.forEach((imagensSelecionadas) => {
+
+        const carta = cartasCriadas(imagensSelecionadas)
+    });
+
+
+}
+
+jogabilidadeCartas()
+
+
+
+
 
 function clickCard(cartaClicada) {
 
-    cartaClicada= document.querySelector('.back')
+    cartaClicada = document.querySelector('.back')
     cartaClicada.classList.toggle('card')
-
 }
+
+// Agora temos que fazer uma copia do html para o JavaScript
+
+
+
+
+
+/*const quantidadeDeCartas = [Number(prompt('Quantas cartas você quer jogar?'))]
+function numeroCartas(){
+    i = quantidadeDeCartas
+
+   console.log(i)
+  
+    
+    
+}
+*/
