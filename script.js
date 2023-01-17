3710118642cae5109d39ff8e6b2f06afc8d9da29
PedroGomes2k <ph.gomes2k@gmail.com.br>
2023-01-17 07:57:59 -0300

    Cartas virando a apresentando figura

--- script.js
+++ script.js
@@ -1,16 +1,119 @@
 
 const containerDasCartas = document.querySelector('.containerCards')
 
+let primeiraCarta = ''
+
+let segundaCarta = ''
+
+let numeroCarta = ''
+
+let cartaCaixa = []
+
 const imagensCartas = [
-    'bobrossparrota',
-    'explodyparrota',
-    'fiestaparrota',
-    'metalparrota',
-    'revertitparrota',
-    'tripletsparrota',
-    'unicornparrota'
+    'bobrossparrot',
+    'explodyparrot',
+    'fiestaparrot',
+    'metalparrot',
+    'revertitparrot',
+    'tripletsparrot',
+    'unicornparrot'
 
 ]
+
+function numerosPares() {
+
+    if (numeroCarta % 2 !== 0 || numeroCarta < 4 || numeroCarta > 14) {
+
+        return true
+
+    } else {
+
+        return false
+    }
+}
+
+function quantidadeCartas() {
+
+    while (numerosPares()) {
+        numeroCarta = Number(prompt('Quantas cartas voce quer jogar? Sua escolha tem que ser entre pares de 4 a 14'))
+    }
+
+    caixaCarta()
+}
+
+
+quantidadeCartas()
+
+
+function caixaCarta() {
+
+    for (let i = 0; i < numeroCarta; i++){
+
+        cartaCaixa.push(imagensCartas[i])
+      
+    }
+    
+    cartasCriadas()
+    
+}
+function jogabilidadeCartas() {
+
+
+    const cartasMisturadas = as.sort(() => Math.random() - 0.5)
+
+    cartasMisturadas.forEach((imagensSelecionadas) => {
+
+        const carta = cartasCriadas(imagensSelecionadas)
+    });
+
+    
+}
+
+
+function verificarCartas() {
+    const primeiraCartaEscolhida = primeiraCarta.getAttribute('data-imagensSelecionada')
+    const segundaCartaEscolhida = segundaCarta.getAttribute('data-imagensSelecionada')
+
+    if (primeiraCartaEscolhida === segundaCartaEscolhida) {
+
+        primeiraCarta = ''
+        segundaCarta = ''
+
+    } else {
+        setTimeout(() => {
+
+            primeiraCarta.classList.remove('rotacao')
+            segundaCarta.classList.remove('rotacao')
+
+            primeiraCarta = ''
+            segundaCarta = ''
+
+        }, 1000)
+    }
+}
+
+function rotacaoCarta({ target }) {
+
+    if (target.parentNode.className.includes('rotacao')) {
+        return
+    }
+
+    if (primeiraCarta === '') {
+
+        target.parentNode.classList.add('rotacao')
+        primeiraCarta = target.parentNode
+
+
+    } else if (segundaCarta === '') {
+
+        target.parentNode.classList.add('rotacao')
+        segundaCarta = target.parentNode
+
+        verificarCartas()
+
+    }
+
+}
 
 function cartasCriadas(imagensSelecionadas) {
 
@@ -30,25 +133,15 @@ function cartasCriadas(imagensSelecionadas) {
     cards.appendChild(front)
     cards.appendChild(back)
 
+    cards.addEventListener('click', rotacaoCarta)
+    cards.setAttribute('data-imagensSelecionada', imagensSelecionadas)
+
     containerDasCartas.appendChild(cards)
+    
 }
 
 
-function jogabilidadeCartas() {
 
-    const cartasDuplicadas = [...imagensCartas, ...imagensCartas]
-
-    const cartasMisturadas = cartasDuplicadas.sort(() => Math.random() -0.2 )
-
-    cartasMisturadas.forEach((imagensSelecionadas) => {
-
-        const carta = cartasCriadas(imagensSelecionadas)
-    });
-
-
-}
-
-jogabilidadeCartas()
 
 
 
@@ -57,22 +150,5 @@ jogabilidadeCartas()
 function clickCard(cartaClicada) {
 
     cartaClicada = document.querySelector('.back')
-    cartaClicada.classList.toggle('card')
+    cartaClicada.classList.add('card')
 }
-
-// Agora temos que fazer uma copia do html para o JavaScript
-
-
-
-
-
-/*const quantidadeDeCartas = [Number(prompt('Quantas cartas você quer jogar?'))]
-function numeroCartas(){
-    i = quantidadeDeCartas
-
-   console.log(i)
-  
-    
-    
-}
-*/
